abf0b1f8fc4c73d64c8226bd0a20faaf1f873df8
MorningStar3600 <54244092+MorningStar3600@users.noreply.github.com>
2021-03-05 18:29:09 +0100

    Initial commit

---
+++ Projet/Projet/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    public struct Cell
+    {
+        private int type;
+        private bool isInFire;
+        private int life;
+
+        #region accessers & getters 
+        public int getType()
+        {
+            return this.type;
+        }
+
+        public void setType(int type) 
+        { 
+            this.type = type; 
+        }
+
+        public bool getIsInFire()
+        {
+            return this.isInFire;
+        }
+
+        public void setIsInFire(bool isInFire)
+        {
+            this.isInFire = isInFire;
+        }
+
+        public int getLife()
+        {
+            return this.life;
+        }
+
+        public void setLife(int life)
+        {
+            this.life = life;
+        }
+        #endregion
+
+        public string getDisplaySymbol()
+        {
+            string rtrn = "";
+            if (this.type == 1)
+            {
+                rtrn = "x";
+            }else if (this.type == 2)
+            {
+                rtrn = "*";
+            }
+            else if (this.type == 3)
+            {
+                rtrn = " ";
+            }
+            else if (this.type == 4)
+            {
+                rtrn = "#";
+            }
+            else if (this.type == 5)
+            {
+                rtrn = "~";
+            }
+            else if (this.type == 6)
+            {
+                rtrn = "O";
+            }
+            else if (this.type == 7)
+            {
+                rtrn = "'";
+            }
+            else if (this.type == 8)
+            {
+                rtrn = ".";
+            }
+
+            return rtrn;
+        }
+
+        public void setRandomValue()
+        {
+            Random rdm = new Random();
+            this.type = rdm.Next(1, 8);
+        }
+    }
+}
---
+++ Projet/Projet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cell[,] map = generateWoods(10, 10);
+            draw(map);
+            Console.ReadKey();
+        }
+
+        static Cell[,] generateWoods(int l, int c)
+        {
+            Cell[,] map = new Cell[c, l];
+            for (int i = 0; i <= map.GetLength(0); i++)
+            {
+                for (int j = 0; j <= map.GetLength(1); j++)
+                {
+                    map[i, j].setRandomValue();
+                }
+            }
+            return map;
+        }
+
+        static void draw(Cell[,] map)
+        {
+            for (int i=0; i<=map.GetLength(0); i++)
+            {
+                for (int j=0; j<=map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j].getDisplaySymbol() + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
