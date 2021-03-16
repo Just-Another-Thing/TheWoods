cdf4173ffecbc28ca2d96ef3992455ba2cbbc55b
MorningStar3600 <54244092+MorningStar3600@users.noreply.github.com>
2021-03-16 12:11:11 +0100

    Mutlithreading

---
+++ Projet/Projet/Draws.cs
@@ -0,0 +1,94 @@
+﻿using Projet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    class Draws
+    {
+        private struct Param
+        {
+            public Cell[,] map;
+            public int IconeType;
+            public int test;
+        }
+
+        public static void Draw(Cell[,] map, int IconeType, int test = 0)
+        {
+            Thread thread = new Thread(DrawThread);
+            Param p = new Param();
+            p.map = map;
+            p.IconeType = IconeType;
+            p.test = test;
+
+            thread.Start((object) p);
+
+        }
+
+
+
+        private static void DrawThread(object p)
+        {
+            Param param = (Param) p;
+            Cell[,] map = param.map;
+            int IconeType = param.IconeType;
+            int test = param.test;
+
+            int maxl = ((Console.WindowWidth - Console.WindowWidth / 3 + Console.WindowWidth / 10) / 2) - 1;
+            int maxh = Console.WindowHeight - 1;
+            int x = maxh - ((maxh + map.GetLength(0)) / 2);
+            int y = maxl - maxl / 2 - map.GetLength(1) / 2;
+            if (test == 0)
+            {
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    Console.SetCursorPosition(y, x / 2 + i + 1);
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        if (map[i, j].GetType() == 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                        }
+                        else if (map[i, j].GetType() == 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                        }
+                        else if (map[i, j].GetType() == 4)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        }
+                        else if (map[i, j].GetType() == 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j].GetIsInFire() == 1 || map[i, j].GetIsInFire() == 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        Console.Write(map[i, j].GetDisplaySymbol(IconeType) + " ");
+                    }
+                }
+            }
+            else if (test == 1)
+            {
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        Console.Write((int)(map[i, j].GetHeight() * 100) + "|");
+                    }
+                    Console.Write("\n");
+                }
+            }
+
+        }
+    }
+}
--- Projet/Projet/Program.cs
+++ Projet/Projet/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
+using System.Threading;
 
 namespace Projet
 {
@@ -27,7 +27,7 @@ namespace Projet
                 activeSlider = Navigation.NavigationManager(SliderList, activeSlider, WoodGenerator.GenerateWoods(55, 29));
                 Navigation.SetDesignNav();
                 Menu.DisplaySlider(SliderList, activeSlider);
-                Draw(Program.map, SliderList[3].GetLastSelect(), 0);
+                Draws.Draw(Program.map, SliderList[3].GetLastSelect(), 0);
             } while (activeSlider != -1);
             
 
@@ -59,63 +59,7 @@ namespace Projet
 
         
 
-        public static void Draw(Cell[,] map, int IconeType, int test = 0)
-        {
-            int maxl = ((Console.WindowWidth - Console.WindowWidth / 3 + Console.WindowWidth / 10) / 2) - 1;
-            int maxh = Console.WindowHeight - 1;
-            int x = maxh - ((maxh+ map.GetLength(0))/2);
-            int y = maxl - maxl/2 - map.GetLength(1)/2;
-            Debug.WriteLine(map.GetLength(0));
-            if (test == 0)
-            {
-                for (int i = 0; i < map.GetLength(0); i++)
-                {
-                    Console.SetCursorPosition(y, x/2+i+1);
-                    for (int j = 0; j < map.GetLength(1); j++)
-                    {
-                        if (map[i, j].GetType() == 5)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                        }
-                        else if (map[i, j].GetType() == 2)
-                        {
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
-                        }
-                        else if (map[i, j].GetType() == 4)
-                        {
-                            Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        }
-                        else if (map[i, j].GetType() == 1)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.White;
-                        }
-                        if (map[i, j].GetIsInFire() == 1 || map[i, j].GetIsInFire() == 2)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                        }
-                        Console.Write(map[i, j].GetDisplaySymbol(IconeType) + " ");
-                    }
-                }
-            }
-            else if (test == 1)
-            {
-                for (int i = 0; i < map.GetLength(0); i++)
-                {
-                    for (int j = 0; j < map.GetLength(1); j++)
-                    {
-                        Console.Write((int)(map[i, j].GetHeight()*100)+"|");
-                    }
-                    Console.Write("\n");
-                }
-            }
-            
-        }
-
-        static void PassTour(Cell[,] map)
+        public static void PassTour(Cell[,] map)
         {
             for (int i = 0; i < map.GetLength(0); i++)
             {
